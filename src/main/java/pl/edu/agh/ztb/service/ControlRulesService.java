bc5a041bd1868f98b9aa9c82cd98e6662b1f8fef
wawek <wawek91@gmail.com>
2015-01-27 18:03:36 +0100

    RestLogger added, changes in libs names, removal of fakeservice

--- src/main/java/pl/edu/agh/ztb/service/ControlRulesService.java
+++ src/main/java/pl/edu/agh/ztb/service/ControlRulesService.java
@@ -1,6 +1,8 @@
 package pl.edu.agh.ztb.service;
 
 import agh.db.ControlRuleDaoImpl;
+import loggers.enums.SourceType;
+import loggers.impl.RestLogger;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -10,6 +12,8 @@ import java.util.List;
 @Path("/control_rules")
 public class ControlRulesService {
 
+    private static final RestLogger logger = new RestLogger();
+    
     @GET
     @Path("/select")
     @Produces(MediaType.APPLICATION_JSON)
@@ -18,9 +22,11 @@ public class ControlRulesService {
         List<String> controlRuleList;
         try{
             controlRuleList = controlRuleDao.getAllControlRules();
+            logger.logSuccess(SourceType.MANUAL, "All Control Rules fetched");
             return Response.ok(controlRuleList).build();
         }
         catch(Exception ex){
+            logger.logFailure(SourceType.MANUAL, ex.getMessage());
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Exception thrown").build();
         }
     }
@@ -33,43 +39,54 @@ public class ControlRulesService {
         try{
             String controlRule = controlRuleDao.getControlRule(name);
             if(controlRule == null){
+                logger.logFailure(SourceType.MANUAL, "Control Rule " + name + " not found");
                 return Response.status(Response.Status.NOT_FOUND).entity("Not found").build();
             }
+            logger.logSuccess(SourceType.MANUAL, controlRule);
             return Response.ok(controlRule).build();
         }
         catch(Exception ex){
+            logger.logFailure(SourceType.MANUAL, ex.getMessage());
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Exception thrown").build();
         }
     }
 
     @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
     @Path("/insert")
-    public Response insertControlRule(@FormParam("name") String name) {
+    public Response insertControlRule(String name) {
         if(name == null || name.trim().isEmpty()){
+            logger.logFailure(SourceType.MANUAL, "Bad request");
             return Response.status(Response.Status.BAD_REQUEST).entity("Bad parameter").build();
         }
         ControlRuleDaoImpl controlRuleDao = new ControlRuleDaoImpl();
         try {
             controlRuleDao.addControlRule(name);
+            logger.logSuccess(SourceType.MANUAL, "Control Rule " + name + " inserted");
             return Response.ok("OK").build();
         }
         catch(Exception ex){
+            logger.logFailure(SourceType.MANUAL, ex.getMessage());
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Exception thrown").build();
         }
     }
 
     @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
     @Path("/delete")
-    public Response deleteControlRule(@FormParam("name") String name) {
+    public Response deleteControlRule(String name) {
         if(name == null || name.trim().isEmpty()){
+            logger.logFailure(SourceType.MANUAL, "Bad request");
             return Response.status(Response.Status.BAD_REQUEST).entity("Bad parameter").build();
         }
         ControlRuleDaoImpl controlRuleDao = new ControlRuleDaoImpl();
         try {
             controlRuleDao.deleteControlRule(name);
+            logger.logSuccess(SourceType.MANUAL, "Control Rule " + name + " deleted");
             return Response.ok("OK").build();
         }
         catch(Exception ex){
+            logger.logFailure(SourceType.MANUAL, ex.getMessage());
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Exception thrown").build();
         }
 
